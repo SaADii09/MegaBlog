b011aa595d0898d31074ae6e6e032f59d4fb9116
SaADii09 <saadahmad6830879@gmail.com>
2024-07-23 16:00:54 +0500

    Fix: Formatting and Semicolon Issues
    Updated code formatting and added semicolons where they were missing. These changes ensure consistency and readability throughout the project.

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,15 +1,15 @@
-import conf from "../conf/conf.js"
-import { Client, Account, ID } from "appwrite"
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-    client = new Client()
-    account
+    client = new Client();
+    account;
 
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId)
-        this.account = new Account(this.client)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
     }
 
     async createAccount({ email, password, name }) {
@@ -19,45 +19,45 @@ export class AuthService {
                 email,
                 password,
                 name
-            )
+            );
             if (userAccount) {
                 // call another method
-                return this.login({ email, password })
+                return this.login({ email, password });
             } else {
-                return userAccount
+                return userAccount;
             }
         } catch (error) {
-            throw error
+            throw error;
         }
     }
 
     async login({ email, password }) {
         try {
-            return await this.account.createEmailSession(email, password)
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
-            throw error
+            throw error;
         }
     }
 
     async getCurrentUser() {
         try {
-            return await this.account.get()
+            return await this.account.get();
         } catch (error) {
-            console.log("Appwrite serive :: getCurrentUser :: error", error)
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
 
-        return null
+        return null;
     }
 
     async logout() {
         try {
-            await this.account.deleteSessions()
+            await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite serive :: logout :: error", error)
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
-const authService = new AuthService()
+const authService = new AuthService();
 
-export default authService
+export default authService;
--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,17 +1,17 @@
-import conf from "../conf/conf.js"
-import { Client, ID, Databases, Storage, Query } from "appwrite"
+import conf from "../conf/conf.js";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
 export class Service {
-    client = new Client()
-    databases
-    bucket
+    client = new Client();
+    databases;
+    bucket;
 
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId)
-        this.databases = new Databases(this.client)
-        this.bucket = new Storage(this.client)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
     }
 
     async createPost({ title, slug, content, featuredImage, status, userId }) {
@@ -27,9 +27,9 @@ export class Service {
                     status,
                     userId,
                 }
-            )
+            );
         } catch (error) {
-            console.log("Appwrite serive :: createPost :: error", error)
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
 
@@ -45,9 +45,9 @@ export class Service {
                     featuredImage,
                     status,
                 }
-            )
+            );
         } catch (error) {
-            console.log("Appwrite serive :: updatePost :: error", error)
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
@@ -57,11 +57,11 @@ export class Service {
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
-            )
-            return true
+            );
+            return true;
         } catch (error) {
-            console.log("Appwrite serive :: deletePost :: error", error)
-            return false
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false;
         }
     }
 
@@ -71,10 +71,10 @@ export class Service {
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug
-            )
+            );
         } catch (error) {
-            console.log("Appwrite serive :: getPost :: error", error)
-            return false
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false;
         }
     }
 
@@ -84,10 +84,10 @@ export class Service {
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries
-            )
+            );
         } catch (error) {
-            console.log("Appwrite serive :: getPosts :: error", error)
-            return false
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false;
         }
     }
 
@@ -99,27 +99,27 @@ export class Service {
                 conf.appwriteBucketId,
                 ID.unique(),
                 file
-            )
+            );
         } catch (error) {
-            console.log("Appwrite serive :: uploadFile :: error", error)
-            return false
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false;
         }
     }
 
     async deleteFile(fileId) {
         try {
-            await this.bucket.deleteFile(conf.appwriteBucketId, fileId)
-            return true
+            await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+            return true;
         } catch (error) {
-            console.log("Appwrite serive :: deleteFile :: error", error)
-            return false
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false;
         }
     }
 
     getFilePreview(fileId) {
-        return this.bucket.getFilePreview(conf.appwriteBucketId, fileId)
+        return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
     }
 }
 
-const service = new Service()
-export default service
+const service = new Service();
+export default service;
